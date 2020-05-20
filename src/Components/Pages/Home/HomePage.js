9e0f932a1e1eafb70c3e7dd4962694032e335a57
veronkrot <veronkrotenko@gmail.com>
2020-05-20 13:40:42 +0300

    updated index.html

---
+++ src/Components/Pages/Home/HomePage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {Header} from "../../Header/Header";
+
+export const Home = () => {
+    return (
+        <div className="home-page">
+            <Header/>
+        </div>
+    )
+}
